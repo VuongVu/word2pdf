b721ead0d7b11752787c593be230180d6ab62cc6
Vuong Vu <vuongvu333@gmail.com>
2020-04-05 09:06:10 +0700

    :triangular_flag_on_post: Update request version, server converter
    Update request version, server converter

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const request = require('request');
-const fs = require('fs');
+const request = require("request");
+const fs = require("fs");
 
-const word2pdf = async path => {
+const word2pdf = async (path) => {
   const buffer = await new Promise((resolve, reject) => {
     fs.readFile(path, (err, data) => {
       if (err) {
@@ -14,13 +14,13 @@ const word2pdf = async path => {
 
   const data = await new Promise((resolve, reject) => {
     const req = request.post(
-      'http://mirror1.convertonlinefree.com',
+      "https://convertonlinefree.com",
       {
         encoding: null,
         headers: {
-          'User-Agent':
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.94 Safari/537.36'
-        }
+          "User-Agent":
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.94 Safari/537.36",
+        },
       },
       (err, res) => {
         if (err) return reject(err);
@@ -28,17 +28,17 @@ const word2pdf = async path => {
       }
     );
     const form = req.form();
-    form.append('__EVENTTARGET', '');
-    form.append('__EVENTARGUMENT', '');
-    form.append('__VIEWSTATE', '');
-    form.append('ctl00$MainContent$fu', buffer, {
-      filename: 'output.docx',
+    form.append("__EVENTTARGET", "");
+    form.append("__EVENTARGUMENT", "");
+    form.append("__VIEWSTATE", "");
+    form.append("ctl00$MainContent$fu", buffer, {
+      filename: "output.docx",
       contentType:
-        'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
     });
 
-    form.append('ctl00$MainContent$btnConvert', 'Convert');
-    form.append('ctl00$MainContent$fuZip', '');
+    form.append("ctl00$MainContent$btnConvert", "Convert");
+    form.append("ctl00$MainContent$fuZip", "");
   });
 
   return data;
